97469bb0659a693952324e3cf35c18742d50dd02
Michael Bunsen <notbot@gmail.com>
2017-01-03 16:22:53 -0800

    Support other formats for viewing request data

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from flask import Flask, request, jsonify, send_file, redirect, url_for
+from flask import Flask, request, send_file, redirect, url_for, jsonify, render_template_string
+from flask.json import JSONEncoder
 from PIL import Image, ImageDraw
 import StringIO
 
@@ -8,15 +10,29 @@ import StringIO
 app = Flask(__name__)
 
 
-def request_headers_str():
-    headers = [u"{}: {}".format(k, v) for k,v in request.headers.items()]
-    #request_vars = request.values
-    headers_str = u"\n\r".join(headers)
-    headers_str = headers_str.encode('utf8')
-    return headers_str
+def request_data():
+    data = {} 
+    data['headers'] = dict(request.headers) # HTTP headers
+    data['query_args'] = dict(request.args)  # GET & POST vars
+    data['environ'] = dict(request.environ)
+    return data
+
+def request_data_str():
+    data = request_data()
+    tmpl = """
+    {% for k,v in data.items() %}{% if v.items %}{{ k }}: 
+    {% for kk, vv in v.items() %}
+    {{ kk }}: {{ vv }}{% endfor %} {% else %} 
+    {{ k }}: {{ v }} {% endif %}
+
+    {% endfor %}
+    """
+    txt =  render_template_string(tmpl, data=data)
+    txt = txt.encode('utf8')
+    return txt
 
 def create_image(txt):
-    image = Image.new("RGBA", (600,150), (255,255,255))
+    image = Image.new("RGBA", (1024,2048), (255,255,255))
     draw = ImageDraw.Draw(image)
 
     draw.text((10, 0), txt, (0,0,0))
@@ -24,16 +40,50 @@ def create_image(txt):
 
 def serve_image(pil_img):
     img_io = StringIO.StringIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
+    pil_img.save(img_io, 'PNG', quality=90)
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
+    return send_file(img_io, mimetype='image/png')
 
-@app.route('/image.jpg')
-def image():
-    headers = request_headers_str()
-    img = create_image(headers) 
+@app.route('/request_data.png')
+def as_image():
+    txt = request_data_str()
+    img = create_image(txt) 
     return serve_image(img)
+
+@app.route('/request_data.html')
+def as_html():
+    data = request_data()
+    tmpl = """
+    <!doctype html>
+    <html><body style="font-family: monospace;">
+    <ul>
+    {% for k,v in data.items() %}
+    {% if v.items %}
+    <li>{{ k }}: 
+    <ul>{% for kk, vv in v.items() %}
+    <li><b>{{ kk }}:</b> {{ vv }}</li>
+    {% endfor %}
+    </ul>
+    {% else %}
+    <li>{{ k }}: {{ v }}</li>
+    {% endif %} 
+    {% endfor %}
+    </ul>
+    </body></html>
+    """
+    return render_template_string(tmpl, data=data)
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, o):
+        return str(o)
+
+app.json_encoder = CustomJSONEncoder
+@app.route('/request_data.json')
+def as_json():
+    data = request_data()
+    return jsonify(data), 200
 
 @app.route('/')
 def index():
-    return redirect(url_for('image'))
+    return redirect(url_for('as_html'))
