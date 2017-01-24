083ff9132993011ad6fc57f6c59802b58f09071a
Michael Bunsen <notbot@gmail.com>
2017-01-24 11:18:09 -0800

    Load sensitive env vars from file at runtime

--- runserver.sh
+++ runserver.sh
@@ -7,6 +7,9 @@ python -c "import sys; assert(sys.version_info[0] < 3)" || { echo 'Only Python 2
 export FLASK_DEBUG=True
 export FLASK_APP=app.py
 
-export AWS_PROFILE=michael
+if [ ! -f .env ]
+then
+    export $(cat .env | xargs)
+fi
 
 flask run
